769f64e9ceaea0caf0da3bfc93e011bab1eabc5c
Scott Borecki <scottborecki@gmail.com>
2021-10-24 14:35:04 -0600

    Add buildMainCharacter function

--- src/book.js
+++ src/book.js
@@ -2,9 +2,19 @@ function createTitle(title) {
   return 'The ' + title;
 }
 
+function buildMainCharacter(name, age, pronouns) {
+  var character = {};
+
+  character.name = name;
+  character.age = age;
+  character.pronouns = pronouns;
+
+  return character;
+}
+
 module.exports = {
   createTitle,
-  // buildMainCharacter,
+  buildMainCharacter,
   // saveReview,
   // calculatePageCount,
   // writeBook,
