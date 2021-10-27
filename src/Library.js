5996f3a78d8c6ee6c337252fe44a426325c95362
Scott Borecki <scottborecki@gmail.com>
2021-10-27 08:24:27 -0600

    Add checkoutBook function

--- src/Library.js
+++ src/Library.js
@@ -16,8 +16,24 @@ function addBook(library, book) {
   library.shelves[book.genre].push(book);
 }
 
+function checkoutBook(library, bookTitle, genre) {
+  var index;
+
+  for (var i = 0; i < library.shelves[genre].length; i++) {
+    if (library.shelves[genre][i].title === bookTitle) {
+      index = i;
+
+      library.shelves[genre].splice(index, 1);
+
+      return 'You have now checked out ' + bookTitle + ' from the ' + library.name;
+    }
+  }
+
+  return 'Sorry, there are currently no copies of ' + bookTitle + ' available at the ' + library.name;
+}
+
 module.exports = {
   createLibrary: createLibrary,
   addBook: addBook,
-  // checkoutBook
+  checkoutBook: checkoutBook
 };
